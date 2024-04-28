b6931657f63511e72529fe306dadb4bb51f1f326
Jack Phoenix <ashley@uncyclomedia.co>
2024-04-28 12:21:31 +0000

    Fix up clickjacking code for MediaWiki 1.41+
    OutputPage#allowClickjacking was removed in MW core commit f33e71f1f42aa378cdd97eaaa39bcf7c5c82f504 (see T305161).
    
    Change-Id: Ic01ab3249014794bf5a846427314c3ff9c579b00
    (cherry picked from commit 124c695c11c6b9c881e22082a8d11f9497028b6f)

--- includes/upload/PictureGameAjaxUploadForm.php
+++ includes/upload/PictureGameAjaxUploadForm.php
@@ -56,8 +56,14 @@ class PictureGameAjaxUploadForm extends UploadForm {
 	 */
 	function displayForm( $submitResult ) {
 		parent::displayForm( $submitResult );
-		if ( method_exists( $this->getOutput(), 'allowClickjacking' ) ) {
-			$this->getOutput()->allowClickjacking();
+		$out = $this->getOutput();
+		if ( method_exists( $out, 'allowClickjacking' ) ) {
+			// Up to MW 1.41
+			// @phan-suppress-next-line PhanUndeclaredMethod
+			$out->allowClickjacking();
+		} else {
+			// MW 1.41+
+			$out->setPreventClickjacking( false );
 		}
 	}
 
